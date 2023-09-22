27ad13be5e17e6c483dfccd258ce2c1ee7f3185e
Wataru Ishikawa <wataruby@gmail.com>
2023-09-22 11:44:02 +0900

    u

--- ans/tessoku/b04.go
+++ ans/tessoku/b04.go
@@ -13,11 +13,11 @@ func main() {
 	A := strings.Split(fmt.Sprintf("%08d", N), "")
 
 	digit := 8
-	ten := 0
+	decimal := 0
 	for i := 0; i < digit; i++ {
 		a, _ := strconv.Atoi(A[i])
-		ten += a * int(math.Pow(2, float64((digit-1)-i)))
+		decimal += a * int(math.Pow(2, float64((digit-1)-i)))
 	}
 
-	fmt.Printf("%d", ten)
+	fmt.Printf("%d", decimal)
 }

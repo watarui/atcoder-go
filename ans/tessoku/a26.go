483cfd8cba7aad453429e07bd2fce50a9b696352
Wataru Ishikawa <wataruby@gmail.com>
2023-09-24 20:30:39 +0900

    u

---
+++ ans/tessoku/a26.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func isPrime(n int, div int) bool {
+	if div == 1 {
+		return true
+	}
+	if n%div == 0 {
+		return false
+	} else {
+		return isPrime(n, div-1)
+	}
+}
+
+func isPrime2(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var Q int
+	fmt.Scan(&Q)
+
+	for i := 0; i < Q; i++ {
+		var X int
+		fmt.Scan(&X)
+
+		if isPrime2(X) {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
+	}
+}

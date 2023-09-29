a02cee985d75076a14b2823acc54cd70d93d6c65
Wataru Ishikawa <wataruby@gmail.com>
2023-09-29 22:35:29 +0900

    u

---
+++ ans/tessoku/b17.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func abs(a int) int {
+	return int(math.Abs(float64(a)))
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func reverse(s []int) []int {
+	l := len(s)
+	for i := 0; i < l/2; i++ {
+		s[i], s[l-i-1] = s[l-i-1], s[i]
+	}
+	return s
+}
+
+func restore(current int, dp, h, V []int) []int {
+	V = append(V, current)
+	if current == 1 {
+		return V
+	}
+	if dp[current-1]+abs(h[current]-h[current-1]) == dp[current] {
+		return restore(current-1, dp, h, V)
+	} else {
+		return restore(current-2, dp, h, V)
+	}
+}
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+
+	dp := make([]int, N+1)
+
+	h := make([]int, N+1)
+	for i := 1; i <= N; i++ {
+		fmt.Scan(&h[i])
+	}
+
+	dp[1] = 0
+	dp[2] = abs(h[2] - h[1])
+	for i := 3; i <= N; i++ {
+		dp[i] = min(dp[i-1]+abs(h[i]-h[i-1]), dp[i-2]+abs(h[i]-h[i-2]))
+	}
+
+	// 復元
+	ans := reverse(restore(N, dp, h, []int{}))
+
+	al := len(ans)
+	fmt.Println(al)
+	for i := 0; i < al; i++ {
+		if i >= 1 {
+			fmt.Print(" ")
+		}
+		fmt.Print(ans[i])
+	}
+}
